9c7c37e4b7448524e4e62ea015bcc4aea338adf1
BliScript <99600292+BliScript@users.noreply.github.com>
2022-08-20 19:56:34 -0300

    persona.model.ts added

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,24 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './components/encabezado/encabezado.component';
-
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { SkillsComponent } from './components/skills/skills.component';
-import { HttpClientModule } from '@angular/common/http';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { ButtonComponent } from './components/button/button.component';
-
 import { FooterComponent } from './components/footer/footer.component';
-
 import { BannerComponent } from './components/banner/banner.component';
-
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-
-
+import { HttpClientModule } from '@angular/common/http';
 
 ;
 
@@ -26,20 +19,15 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
   declarations: [
     AppComponent,
     EncabezadoComponent,
-    
     ExperienciaComponent,
     SkillsComponent,
     EducacionComponent,
     ProyectosComponent,
     EncabezadoComponent,
     ButtonComponent,
-  
     FooterComponent,
- 
     BannerComponent,
-
     AcercaDeComponent,
-
   
   ],
   imports: [
