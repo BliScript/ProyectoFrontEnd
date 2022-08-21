840c9d3ebb4e288165be1d22d6d42eda974866f6
BliScript <99600292+BliScript@users.noreply.github.com>
2022-08-21 12:24:21 -0300

    nombre+ap db

--- FrontEnd/src/app/service/persona.service.ts
+++ FrontEnd/src/app/service/persona.service.ts
@@ -6,14 +6,19 @@ import { persona } from '../model/persona.model';
 @Injectable({
   providedIn: 'root'
 })
+
+//imp backend **************************************
 export class PersonaService {
 
-  //imp backend **************************************
-  URL = 'http://localhost:8080/personas/';
+  URL = 'http://localhost:8080/personas';
 
   constructor(private http: HttpClient) { }
 
-  // public getPersona(): Observable<persona> {}
+  public getPersona():Observable<persona> { 
+    return this.http.get<persona>(this.URL+'/traer/info'); //trae mi nombre y apellido de db > postman
+  }
+
+  
     
  
 }
